222f2c91d6aae084e96f79e4294de1be8d246694
vanekt <vanekt@yandex.ru>
2025-03-27 10:36:49 -0300

    open browser when dev server starts

--- example/src/cases/index.ts
+++ example/src/cases/index.ts
@@ -43,4 +43,74 @@ export default [
     },
     vars: { name: "Rendero" },
   },
+  {
+    index: "2",
+    title: "Второй кейс",
+    modules: [initMyModule],
+    json: {
+      module: "my",
+      type: "div",
+      props: {
+        id: "my-div",
+        title: "My DIV",
+        style: "display: flex; flex-direction: column;",
+      },
+      children: [
+        {
+          module: "my",
+          type: "span",
+          props: {
+            style: "color: green;",
+          },
+          children: [
+            {
+              module: "my",
+              type: "hello",
+              props: { value: "Green" },
+            },
+          ],
+        },
+
+        {
+          module: "my",
+          type: "div",
+          props: {
+            id: "my-div2",
+            title: "My DIV2",
+            style: "display: flex; flex-direction: column;",
+          },
+          children: [
+            {
+              module: "my",
+              type: "span",
+              props: {
+                style: "color: pink;",
+              },
+              children: [
+                {
+                  module: "my",
+                  type: "hello",
+                  props: { value: "Pink" },
+                },
+              ],
+            },
+          ],
+        },
+
+        {
+          module: "my",
+          type: "hello",
+        },
+
+        {
+          module: "my",
+          type: "eval",
+          props: {
+            code: 'console.log("Hi, {{name}}!")',
+          },
+        },
+      ],
+    },
+    vars: { name: "Rendero" },
+  },
 ];
